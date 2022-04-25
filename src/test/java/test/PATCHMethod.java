3035be609755e5206592c6cf0dbdbacbed8d2f08
LuongNguyen1995 <luong.epu.dtvt1@gmail.com>
2022-04-25 21:23:47 +0700

    Update PATCH Method

---
+++ src/test/java/test/PATCHMethod.java
@@ -0,0 +1,31 @@
+package test;
+
+
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import model.BuildModelJSON;
+import model.PostBody;
+import model.RequestCapability;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class PATCHMethod implements RequestCapability {
+    public static void main(String[] args) {
+        String baseUri = "https://jsonplaceholder.typicode.com";
+
+        //Form up request instance, header and baseUri
+        RequestSpecification request = given();
+        request.baseUri(baseUri);
+        request.header(defaultHeader);
+
+        //Form up body
+        PostBody postBody = new PostBody();
+        postBody.setTitle("New Title");
+        String postBodyStr = BuildModelJSON.parseJSONString(postBody);
+        final String TARGET_POST_ID = "1";
+        Response response = request.body(postBodyStr).patch("/posts/".concat(TARGET_POST_ID));
+        response.then().body("title", equalTo(postBody.getTitle()));
+        response.prettyPrint();
+    }
+}
